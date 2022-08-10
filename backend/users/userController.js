85c47bae091525f1ccc18bca4c6ada5160d2df00
yousafsabir <yousafsabir430516@gmail.com>
2022-08-10 11:46:08 +0500

    register and login with jwt

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,23 +1,98 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("./userModel");
 
-const registerUser = asyncHandler((req, res) => {
-    res.json({
-        status: 200,
-        message: "user registered successfully",
+const registerUser = asyncHandler(async (req, res) => {
+    // destructuring
+    const { name, email, password } = req.body;
+    // Check if we have all the values
+    if (!name || !email || !password) {
+        res.status(400).json({
+            message: "please include all fields",
+        });
+    }
+
+    // check if user already exists
+    const userExists = User.findOne({ email });
+    if (userExists) {
+        res.status(400).json({
+            message: "user Already exists",
+        });
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPass,
     });
+    if (user) {
+        res.status(201).json({
+            message: "user registered successfully",
+            user: {
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user.id),
+            },
+        });
+    } else {
+        res.status(400).json({
+            message: "Invalid user data",
+        });
+    }
 });
-const loginUser = asyncHandler((req, res) => {
-    res.json({
-        status: 200,
-        message: "user logged in successfully",
-    });
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).json({
+            message: "please include all fields",
+        });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        res.status(400).json({
+            message: "User doesn't exist",
+        });
+    }
+
+    const passMatch = await bcrypt.compare(password, user.password);
+
+    if (passMatch) {
+        res.status(200).json({
+            message: "user logged in",
+            user: {
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user.id),
+            },
+        });
+    } else {
+        res.status(400).json({
+            message: "wrong password",
+        });
+    }
 });
-const getMe = asyncHandler((req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({
         status: 200,
         message: "user data sent successfully",
     });
 });
+
+// Generate JWT Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "5d",
+    });
+};
 
 module.exports = {
     registerUser,
